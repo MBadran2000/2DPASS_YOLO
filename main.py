f831794fedb99bc0c15703aaf5fa3a5f1b25dd46
MBadran2000 <58951181+MBadran2000@users.noreply.github.com>
2023-05-21 23:23:02 +0300

    save all checkpoints

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def parse_config():
     parser.add_argument('--log_dir', type=str, default='default', help='log location')
     parser.add_argument('--monitor', type=str, default='val/mIoU', help='the maximum metric')
     parser.add_argument('--stop_patience', type=int, default=50, help='patience for stop training')
-    parser.add_argument('--save_top_k', type=int, default=1, help='save top k checkpoints, use -1 to checkpoint every epoch')
+    parser.add_argument('--save_top_k', type=int, default=-1, help='save top k checkpoints, use -1 to checkpoint every epoch')
     parser.add_argument('--check_val_every_n_epoch', type=int, default=1, help='check_val_every_n_epoch')
     parser.add_argument('--SWA', action='store_true', default=False, help='StochasticWeightAveraging')
     parser.add_argument('--baseline_only', action='store_true', default=False, help='training without 2D')
